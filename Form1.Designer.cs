5c6e858476f1c4246c1809c62d9b79462efb12a1
Vinic <rickwesselink1@hotmail.com>
2015-03-05 21:55:12 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Lingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.woordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.gegenereerdWoordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(47, 81);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(17, 26);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.Text = ".";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox8.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(70, 81);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(17, 26);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.Text = ".";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox9.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(93, 81);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(17, 26);
+            this.textBox9.TabIndex = 10;
+            this.textBox9.Text = ".";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox10.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox10.Location = new System.Drawing.Point(116, 81);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(17, 26);
+            this.textBox10.TabIndex = 9;
+            this.textBox10.Text = ".";
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox11.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox11.Location = new System.Drawing.Point(137, 81);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(17, 26);
+            this.textBox11.TabIndex = 8;
+            this.textBox11.Text = ".";
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            this.textBox12.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox12.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox12.Location = new System.Drawing.Point(158, 81);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(17, 26);
+            this.textBox12.TabIndex = 7;
+            this.textBox12.Text = ".";
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(47, 49);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(17, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(70, 49);
+            this.textBox2.MaxLength = 1;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(17, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(93, 49);
+            this.textBox3.MaxLength = 1;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(17, 26);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(116, 49);
+            this.textBox4.MaxLength = 1;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(17, 26);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(137, 49);
+            this.textBox5.MaxLength = 1;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(17, 26);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(158, 49);
+            this.textBox6.MaxLength = 1;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(17, 26);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(181, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 58);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(285, 26);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.woordToolStripMenuItem,
+            this.gegenereerdWoordToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Padding = new System.Windows.Forms.Padding(2, 0, 4, 0);
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(54, 22);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // woordToolStripMenuItem
+            // 
+            this.woordToolStripMenuItem.Name = "woordToolStripMenuItem";
+            this.woordToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.woordToolStripMenuItem.Text = "Woord";
+            this.woordToolStripMenuItem.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 4);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 2, 0, 6);
+            this.label1.Size = new System.Drawing.Size(133, 26);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Aantal keer geraden";
+            // 
+            // textBox13
+            // 
+            this.textBox13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox13.Enabled = false;
+            this.textBox13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox13.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox13.Location = new System.Drawing.Point(245, 4);
+            this.textBox13.Multiline = true;
+            this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
+            this.textBox13.Size = new System.Drawing.Size(40, 26);
+            this.textBox13.TabIndex = 16;
+            this.textBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gegenereerdWoordToolStripMenuItem
+            // 
+            this.gegenereerdWoordToolStripMenuItem.Name = "gegenereerdWoordToolStripMenuItem";
+            this.gegenereerdWoordToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.gegenereerdWoordToolStripMenuItem.Text = "Gegenereerd woord";
+            this.gegenereerdWoordToolStripMenuItem.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 152);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(301, 190);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.Text = "Lingo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem woordToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.ToolStripMenuItem gegenereerdWoordToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lingo
+{
+    public partial class Form1 : Form
+    {
+        int guesses = 0;
+        List<string> sLingoAnswer = new List<string>();
+        List<string> sLingoArr = new List<string>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            openInput();
+            sLingoArr.Add("vissen");
+            sLingoArr.Add("banaan");
+            sLingoArr.Add("afhaal");
+            sLingoArr.Add("banken");
+            sLingoArr.Add("bedrog");
+            sLingoArr.Add("bewust");
+            sLingoArr.Add("braken");
+            sLingoArr.Add("chalet");
+            sLingoArr.Add("achten");
+            sLingoArr.Add("altijd");
+            sLingoArr.Add("zekers");
+            sLingoArr.Add("cursus");
+            sLingoArr.Add("cellen");
+            sLingoArr.Add("cabine");
+            sLingoArr.Add("regios");
+            sLingoArr.Add("ruilen");
+        }
+        
+        private void openInput()
+        {
+            this.sLingoAnswer.Clear();
+            Form2 Lingoinput = new Form2();
+            Lingoinput.ShowDialog();
+            string sAnswerinput = Lingoinput.textBox1.Text;
+            foreach (char sChar in sAnswerinput)
+            {
+                sLingoAnswer.Add("" + sChar);
+            }
+        }
+
+        private void genLingo()
+        {
+            Random r = new Random();
+            int rand = r.Next(0, sLingoArr.Count);
+            string sAnswerinput = sLingoArr[rand];
+            foreach (char sChar in sAnswerinput)
+            {
+                sLingoAnswer.Add("" + sChar);
+            }
+            System.Console.WriteLine(sAnswerinput);
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e) { openInput(); }
+
+        private void menuItem2_Click(object sender, EventArgs e) { genLingo();  }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e) { SendKeys.Send("{TAB}"); }
+
+        private void button1_Click(object sender, EventArgs e)
+        {               
+            List<string> sUserinput = new List<string>();
+            TextBox[] LTinput = new TextBox[] { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6 };
+            TextBox[] LToutput = new TextBox[] { textBox7, textBox8, textBox9, textBox10, textBox11, textBox12 };
+
+            foreach (TextBox item in LTinput)
+            {
+                if (string.IsNullOrEmpty(item.Text))
+                {
+                    item.BackColor = Color.Red;
+                    return;
+                }
+                else
+                {
+                    sUserinput.Add(item.Text);
+                    item.BackColor = Color.White;
+                    item.Clear();
+                }
+                
+            }
+
+            int iu = 0;
+            foreach (TextBox item in LToutput)
+            {
+                item.Text = sUserinput[iu];
+                iu++;  
+            }
+
+            int ia = 0;
+            foreach (TextBox item in LToutput)
+            {
+                try
+                {
+                    if (sUserinput[ia] == sLingoAnswer[ia])
+                    {
+                        item.BackColor = Color.Green;
+                    }
+                    else if (sLingoAnswer.Contains(sUserinput[ia]))
+                    {
+                        item.BackColor = Color.Red;           
+                    }
+                    else
+                    {
+                        item.BackColor = Color.White;
+                    }
+                    
+                    ia++;
+                }
+                catch(Exception z)
+                {
+                    System.Console.WriteLine("Er ging iets fout, probeer het opnieuw: " + z);
+                }               
+            }
+            System.Console.WriteLine("U heeft op de knop gedrukt.");
+            guesses++;
+            textBox13.Text = "" + guesses;
+        }
+    
+    }
+
+}
